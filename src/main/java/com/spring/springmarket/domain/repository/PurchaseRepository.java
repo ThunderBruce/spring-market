0c6acce19b188f0ccbd040aecd03a52a03c51956
Thunder <bruceleonel@gmail.com>
2022-04-27 12:55:40 +0200

    Creando y mapeando el dominio de compras

---
+++ src/main/java/com/spring/springmarket/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.spring.springmarket.domain.repository;
+
+import com.spring.springmarket.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
