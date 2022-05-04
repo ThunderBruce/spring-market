17ada8194b807046ea065ba3c5f4fb8b7c519986
Thunder <bruceleonel@gmail.com>
2022-05-04 17:42:32 +0200

    Autenticacion con JWT

---
+++ src/main/java/com/spring/springmarket/web/controller/AuthController.java
@@ -0,0 +1,42 @@
+package com.spring.springmarket.web.controller;
+
+import com.spring.springmarket.domain.dto.AuthenticationRequest;
+import com.spring.springmarket.domain.dto.AuthenticationResponse;
+import com.spring.springmarket.domain.service.SpringUserDetailsService;
+import com.spring.springmarket.web.security.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private SpringUserDetailsService springUserDetailsService;
+    @Autowired
+    private JWTUtil jwtUtil;
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> createToken(
+            @RequestBody AuthenticationRequest request) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                    request.getUsername(), request.getPassword()));
+            UserDetails userDetails = springUserDetailsService.loadUserByUsername(
+                    request.getUsername());
+            String jwt = jwtUtil.generateToken(userDetails);
+            return new ResponseEntity<>(new AuthenticationResponse(jwt), HttpStatus.OK);
+        } catch (BadCredentialsException e) {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+    }
+}
--- src/main/java/com/spring/springmarket/web/security/SecurityConfig.java
+++ src/main/java/com/spring/springmarket/web/security/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.spring.springmarket.web.security;
 
 import com.spring.springmarket.domain.service.SpringUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -13,5 +16,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(springUserDetailsService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /*Todas las peticiones sin importar lo que haya antes*/
+        http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").
+                permitAll().anyRequest().authenticated();
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
