747daf7d5435036eb70b76bcba7f56e1d59d4cbe
Thunder <bruceleonel@gmail.com>
2022-05-05 20:54:48 +0200

    Autorizacion con JWT, mirar la clase JWTUtil para ver el algoritmo que se usa

--- src/main/java/com/spring/springmarket/web/security/SecurityConfig.java
+++ src/main/java/com/spring/springmarket/web/security/SecurityConfig.java
@@ -1,18 +1,24 @@
 package com.spring.springmarket.web.security;
 
 import com.spring.springmarket.domain.service.SpringUserDetailsService;
+import com.spring.springmarket.web.security.filter.JwtFilterRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private SpringUserDetailsService springUserDetailsService;
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(springUserDetailsService);
@@ -22,7 +28,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         /*Todas las peticiones sin importar lo que haya antes*/
         http.csrf().disable().authorizeRequests().antMatchers("/**/authenticate").
-                permitAll().anyRequest().authenticated();
+                permitAll().anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/v2/api-docs", "/configuration/ui",
+                "/swagger-resources/**", "/configuration/security",
+                "/swagger-ui/**", "/webjars/**");
     }
 
     @Override
