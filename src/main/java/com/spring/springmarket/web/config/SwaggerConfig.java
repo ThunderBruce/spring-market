ffb18f67de0d72e77b08a21325c1c0b2b3df3852
Thunder <bruceleonel@gmail.com>
2022-05-05 21:15:42 +0200

    Swagger con Authorization

--- src/main/java/com/spring/springmarket/web/config/SwaggerConfig.java
+++ src/main/java/com/spring/springmarket/web/config/SwaggerConfig.java
@@ -6,9 +6,11 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -16,6 +18,7 @@ public class SwaggerConfig {
     @Bean
     public Docket api(){
         return new Docket(DocumentationType.SWAGGER_2)
+                .securitySchemes(Arrays.asList(apiKey()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.spring.springmarket.web.controller"))
                 .paths(PathSelectors.regex("/.*"))
@@ -28,5 +31,9 @@ public class SwaggerConfig {
                 .version("1.0.0")
                 .licenseUrl("https://apache.org/licenses/LICENSE-2.0.html")
                 .build();
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT","Authorization","header");
     }
 }
